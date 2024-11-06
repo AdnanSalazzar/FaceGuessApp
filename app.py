f35061015538079490be1ba4f131dbe4524928dd
Adnan Imran <imranadnan99@gmail.com>
2024-11-06 12:51:27 +0600

    Update app.py

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def upload_image():
         return render_template('index.html', result=result , face_data = face_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True , host='0.0.0.0')
